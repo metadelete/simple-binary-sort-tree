dcd87af6642c7882b4ae48182b7fd9735acf6042
Birtan Taşkın <engineerbirtan@gmail.com>
2017-11-05 02:38:22 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+typedef struct TREE_NODE {
+  struct TREE_NODE* right;
+  struct TREE_NODE* left;
+  struct TREE_NODE* parent;
+  int value;
+} TREE_NODE;
+
+int MAX_NODE = 10;
+TREE_NODE *parent = NULL;
+TREE_NODE *crawler = NULL;
+TREE_NODE *trail = NULL;
+int kontrol = 0;
+
+void insert_node(TREE_NODE **node, int);
+static int _rand(){return rand()%20 + 1;}
+int main()
+{
+	srand(time(NULL));
+
+	int *array;
+	array = (int*)malloc(sizeof(int) * MAX_NODE);
+
+  	TREE_NODE* node = NULL;
+  	int counter = 0;
+
+  	while(counter < MAX_NODE) {
+  		array[counter] = _rand();
+  		printf("%d\n", array[counter]);
+  		counter++;
+  	}
+  	counter = 0;
+  	while(counter < MAX_NODE) {
+  		node = (TREE_NODE*)malloc(sizeof(TREE_NODE));
+  		insert_node(&node ,array[counter]);
+  		crawler = trail = parent;
+  		counter++;
+  	}
+  	printf("%s\n", "the end");
+}
+void insert_node(TREE_NODE **node, int value)
+{
+	if(parent == NULL) {
+		TREE_NODE *child = (TREE_NODE*)malloc(sizeof(TREE_NODE));
+		child->value = value;
+		child->left = NULL;
+		child->right = NULL;
+		parent = child;
+		return;
+	} else {
+		if(crawler == NULL) {
+			TREE_NODE *child = (TREE_NODE*)malloc(sizeof(TREE_NODE));
+			child->value = value;
+			child->left = NULL;
+			child->right = NULL;
+			*node = child;
+			switch(kontrol){case 0:trail->left = crawler;break;case 1:trail->right = crawler;break;}
+			return;
+		} else {
+			if(value <= crawler->value) {
+				trail = crawler;
+				crawler = crawler->left;
+				kontrol = 0;
+				insert_node(&(crawler), value);
+			} else {
+				trail = crawler;
+				crawler = crawler->right;
+				kontrol = 1;
+				insert_node(&(crawler), value);
+			}
+		}		
+	}
+}
